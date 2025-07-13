6671d97a29e4522f2284f07d9cf3adf28b9f78d0
ajay <ajaykumar@digitalocean.com>
2025-07-13 04:39:47 +0000

    .

---
+++ functions/submit/index.js
@@ -0,0 +1,10 @@
+// functions/submit/index.js
+
+export default async function (req, context) {
+  const { name } = await req.json();
+  return new Response(JSON.stringify({
+    message: `Thank you ${name} for submitting the details.`
+  }), {
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
