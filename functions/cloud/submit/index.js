96d8dbafb199996b432543e56d37b41d384b8c59
ajay <ajaykumar@digitalocean.com>
2025-07-14 13:57:56 +0000

    .

--- functions/cloud/submit/index.js
+++ functions/cloud/submit/index.js
@@ -1,7 +1,7 @@
 exports.main = async (context) => {
-  const name = context.req.body.name || "Guest";
+  const name = context.req.body?.name || "Guest";
   return {
     statusCode: 200,
-    body: `Thanks, ${name}`
+    body: `Thanks, ${name}`,
   };
 };
